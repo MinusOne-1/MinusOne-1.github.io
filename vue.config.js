d660703268b5c16f7344adc0584d5d8328ffbbe0
Minus One <LAnnaIg03@yandex.ru>
2022-06-18 04:41:10 +0300

    deploying as a page

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,40 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      // точка входа для страницы
+      entry: 'src/main.ts',
+      // исходный шаблон
+      template: 'public/index.html',
+      // в результате будет dist/index.html
+      filename: 'index.html',
+      // когда используется опция title, то <title> в шаблоне
+      // должен быть <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Personal Page',
+      // все фрагменты, добавляемые на этой странице, по умолчанию
+      // это извлечённые общий фрагмент и вендорный фрагмент.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+
+    apiUser: {
+      // точка входа для страницы
+      entry: 'src/main.ts',
+      // исходный шаблон
+      template: 'public/index.html',
+      // в результате будет dist/index.html
+      filename: 'index.html',
+      // когда используется опция title, то <title> в шаблоне
+      // должен быть <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Personal Page',
+      // все фрагменты, добавляемые на этой странице, по умолчанию
+      // это извлечённые общий фрагмент и вендорный фрагмент.
+      chunks: ['chunk-vendors', 'chunk-common', 'index']
+    },
+    // когда используется строковый формат точки входа, то
+    // шаблон будет определяться как `public/subpage.html`,
+    // а если таковой не будет найден, то `public/index.html`.
+    // Выходное имя файла будет определено как `subpage.html`.
+
+  }
 })
